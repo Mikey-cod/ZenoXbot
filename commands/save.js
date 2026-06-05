274ae7eb5710c8c62942b14d330ec96ac935fbed
Mikey-cod <mathiasghislain11@gmail.com>
2026-06-05 19:38:12 +0100

    Create save.js

---
+++ commands/save.js
@@ -0,0 +1,18 @@
+import stylizedChar from "../utils/fancy.js"
+
+export async function pingTest(client, message) {
+    const remoteJid = message.key.remoteJid
+    const start = Date.now()
+
+    await client.sendMessage(remoteJid, { text: "📡 Pinging..." }, { quoted: message })
+
+    const latency = Date.now() - start
+
+    await client.sendMessage(remoteJid, {
+        text: stylizedChar(
+            `🚀 Digital Crew Network\n\n` +
+            `Latency: ${latency} ms\n\n` +
+            `✿🌸𝑴𝒂𝒕𝒉𝒊𝒂𝒔 𝑵𝒐𝒓𝒎𝒂𝒏`
+        )
+    }, { quoted: message })
+}
